bcd9fac1c05b958d234e0c0e58cb62e60c04dc93
Pramesh Pradhan <pra.mesh.dhan@hotmail.com>
2025-05-06 21:27:54 +0545

    Seprate forget and reset password

--- client/src/components/ui/resend-otp.tsx
+++ client/src/components/ui/resend-otp.tsx
@@ -1,5 +1,5 @@
 import { forgetPassword } from "@/services/auth";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const ResendOTP = ({
   email,
