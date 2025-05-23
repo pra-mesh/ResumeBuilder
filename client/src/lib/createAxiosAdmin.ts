bafc3aab2ecd005da605bce31d0c658cecd90325
Pramesh Pradhan <49274805+pra-mesh@users.noreply.github.com>
2025-05-23 06:29:43 +0545

    Update Axios Admin with Request Queue

--- client/src/lib/createAxiosAdmin.ts
+++ client/src/lib/createAxiosAdmin.ts
@@ -3,7 +3,20 @@ import { API_URL, URLS } from "@/constants";
 import { decodeJWT } from "@/lib/jwt";
 import { UserInfo } from "@/interface/UserInfoProps";
 
-//could you please explains this once again
+let isRefreshing = false;
+let failedQueue: any[] = [];
+
+const processQueue = (error: any, token: string | null = null) => {
+  failedQueue.forEach((prom) => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve(token);
+    }
+  });
+
+  failedQueue = [];
+};
 
 export const createAxiosAdmin = (
   getAuth: () => { accessToken: string | null; refreshToken: string | null },
@@ -18,10 +31,7 @@ export const createAxiosAdmin = (
     baseURL: API_URL,
     timeout: 6000,
   });
-  //TODO: Backend ma set api_limit
-  //TODO: How to get role
 
-  //Request Interceptor
   axiosAdminInstance.interceptors.request.use(
     (config) => {
       const { accessToken } = getAuth();
@@ -33,19 +43,34 @@ export const createAxiosAdmin = (
     (error) => Promise.reject(error)
   );
 
-  //Response Interceptor
   axiosAdminInstance.interceptors.response.use(
     (response) => response,
     async (err) => {
       const originalRequest = err.config;
       const { accessToken, refreshToken } = getAuth();
+
       if (
         refreshToken &&
         err.response?.status === 500 &&
         !originalRequest._retry
       ) {
+        if (isRefreshing) {
+          try {
+            const token = await new Promise((resolve, reject) => {
+              failedQueue.push({ resolve, reject });
+            });
+            originalRequest.headers.access_token = token;
+            return axiosAdminInstance(originalRequest);
+          } catch (err) {
+            return Promise.reject(err);
+          }
+        }
+
         originalRequest._retry = true;
+        isRefreshing = true;
+
         const { data }: any = decodeJWT(accessToken);
+        
         try {
           const res = await axios.post(`${URLS.Auth}/refresh`, {
             refresh_token: refreshToken,
@@ -60,14 +85,19 @@ export const createAxiosAdmin = (
             avatar: "",
             role: data?.roles,
           };
+
           onTokenRefresh?.(newAccess, newRefresh, user);
+          
           originalRequest.headers.access_token = newAccess;
+          processQueue(null, newAccess);
+          
           return axiosAdminInstance(originalRequest);
-        } catch (e) {
-          //FIXME: Logout and error handling
-          console.log(e);
+        } catch (error) {
+          processQueue(error, null);
           logout();
-          //return Promise.reject(e);
+          return Promise.reject(error);
+        } finally {
+          isRefreshing = false;
         }
       }
       return Promise.reject(err);
