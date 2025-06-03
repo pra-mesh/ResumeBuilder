787d3d876225204fcc7955ef38c4b321006e04b9
Pramesh Pradhan <pra.mesh.dhan@hotmail.com>
2025-06-03 13:44:57 +0545

    Delete old Resume form

--- client/src/components/ResumeOld/steps/forms/skillsFields.ts
+++
@@ -1,9 +0,0 @@
-const skillsFields = [
-  {
-    name: "name",
-    placeholder: "e.g., JavaScript, Project Management, etc.",
-    type: "text",
-    className: "sm:col-span-2",
-  },
-];
-export default skillsFields;
